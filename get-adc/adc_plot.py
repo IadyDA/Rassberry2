c63d1d0459cad0ca302979362a7d0d8296c87e6b
IadyDA <khabibullina.dm@phystech.edu>
2025-10-21 16:53:59 +0300

    final?

---
+++ get-adc/adc_plot.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+
+def plot_voltage_vs_time(time, voltage, max_voltage):
+    plt.figure(figsize=(10, 6))
+    plt.plot(time, voltage)
+    plt.xlabel('Время, с')
+    plt.ylable('Напряжение, В')
+    plt.title('График зависимости напряжения на входе АЦП от времени')
+    plt.grid()
+    plt.show()
