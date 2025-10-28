0f0c7a607ef80da0b94f61f5aca301723d669bed
IadyDA <khabibullina.dm@phystech.edu>
2025-10-28 15:21:20 +0300

    the lair of scylla

--- get-adc/adc_plot.py
+++ get-adc/adc_plot.py
@@ -4,7 +4,7 @@ def plot_voltage_vs_time(time, voltage, max_voltage):
     plt.figure(figsize=(10, 6))
     plt.plot(time, voltage)
     plt.xlabel('Время, с')
-    plt.ylable('Напряжение, В')
+    plt.ylabel('Напряжение, В')
     plt.title('График зависимости напряжения на входе АЦП от времени')
     plt.grid()
     plt.show()
---
+++ get-adc/r2r-sc.py
@@ -0,0 +1,26 @@
+import RPi.GPIO as GPIO
+import time
+# import matplotlib.pyplot as plt
+import r2r_adc as r2r
+import adc_plot as plt
+
+adc = r2r.R2R_ADC(3.3, compare_time = 0.01, verbose = False)
+
+voltage_values = []
+time_values = []
+duration = 3.0
+
+if __name__ == '__main__':
+    try:
+        start_time = time.time()
+        current_time = time.time()
+
+        while current_time - start_time < duration:
+            voltage_values.append(adc.get_sc_voltage())
+            time_values.append(current_time - start_time)
+            current_time = time.time()
+        
+        plt.plot_voltage_vs_time(time_values, voltage_values, 3.3)
+
+    finally:
+        adc.deinit()
